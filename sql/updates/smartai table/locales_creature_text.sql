ea5ccf924b5f429e8a5be57bf491f9302a646e36
mathman <none@none>
2015-08-29 00:09:12 +0200

    Core/Smartai : Add missing table

---
+++ sql/updates/smartai table/locales_creature_text.sql
@@ -0,0 +1,20 @@
+/*
+SQLyog Community v12.12 (64 bit)
+MySQL - 5.6.26-log 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `locales_creature_text` (
+	`entry` mediumint (8),
+	`groupid` tinyint (3),
+	`id` tinyint (3),
+	`text_loc1` text ,
+	`text_loc2` text ,
+	`text_loc3` text ,
+	`text_loc4` text ,
+	`text_loc5` text ,
+	`text_loc6` text ,
+	`text_loc7` text ,
+	`text_loc8` text 
+); 
